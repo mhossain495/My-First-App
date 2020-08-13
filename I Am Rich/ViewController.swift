512a5ed0847dd91fc4502cf56749455f62a47f99
Mohammed Hossain <MH@shanaazs-Air.fios-router.home>
2020-08-13 18:46:51 -0400

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Mohammed Hossain on 8/2/20.
+//  Copyright © 2020 Mohammed Hossain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
